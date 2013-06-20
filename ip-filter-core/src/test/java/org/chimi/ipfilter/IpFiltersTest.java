ebc242df845151caee8ae2503c11dd5d715b7f74
madvirus <madvirus@madvirus.net>
2013-06-20 16:25:35 +0900

    delete non-required assert

--- ip-filter-core/src/test/java/org/chimi/ipfilter/IpFiltersTest.java
+++ ip-filter-core/src/test/java/org/chimi/ipfilter/IpFiltersTest.java
@@ -2,21 +2,18 @@ package org.chimi.ipfilter;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class IpFiltersTest {
     @Test
     public void createIpFilterWithConfig() {
         IpFilter filter = IpFilters.create(createConfig());
-        assertFalse(filter instanceof CachedIpFilter);
         assertTrue(filter instanceof ConfigIpFilter);
     }
 
     @Test
     public void createCachedIpFilterWithConfig() {
         IpFilter filter = IpFilters.createCached(createConfig());
-        assertFalse(filter instanceof ConfigIpFilter);
         assertTrue(filter instanceof CachedIpFilter);
     }
 
